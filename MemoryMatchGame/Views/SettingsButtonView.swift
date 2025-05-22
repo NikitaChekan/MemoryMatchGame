7ff327010d0f03801629b9943e4b90e795bb8443
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2025-05-22 17:29:33 +0300

    added settingButtonsView

---
+++ MemoryMatchGame/Views/SettingsButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsButtonView.swift
+//  MemoryMatchGame
+//
+//  Created by jopootrivatel on 22.05.2025.
+//
+
+import SwiftUI
+
+struct SettingsButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsButtonView()
+}
