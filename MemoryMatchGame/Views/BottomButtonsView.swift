50a87718adf688f18b2aa0bb9f731040db213e8a
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2025-05-22 13:49:51 +0300

    added WinView and work with SFSafariViewController

---
+++ MemoryMatchGame/Views/BottomButtonsView.swift
@@ -0,0 +1,18 @@
+//
+//  BottomButtonsView.swift
+//  MemoryMatchGame
+//
+//  Created by jopootrivatel on 21.05.2025.
+//
+
+import SwiftUI
+
+struct BottomButtonsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottomButtonsView()
+}
---
+++ MemoryMatchGame/Views/WinGameView.swift
@@ -0,0 +1,18 @@
+//
+//  WinGameView.swift
+//  MemoryMatchGame
+//
+//  Created by jopootrivatel on 21.05.2025.
+//
+
+import SwiftUI
+
+struct WinGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WinGameView()
+}
