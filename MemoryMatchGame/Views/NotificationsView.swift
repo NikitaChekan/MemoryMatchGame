2ad748aa1ba4c0b445af50a95dfc6563f594ec39
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2025-05-22 14:50:59 +0300

    added NotificationView

---
+++ MemoryMatchGame/Views/NotificationsView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationsView.swift
+//  MemoryMatchGame
+//
+//  Created by jopootrivatel on 22.05.2025.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationsView()
+}
